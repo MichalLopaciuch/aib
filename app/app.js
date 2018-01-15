c9980530b5c5a0f4c48aafdcececb0e52fd450c6
Michał Łopaciuch <michal@MacBook-Air-Micha.local>
2018-01-15 22:15:13 +0100

    added started page

--- app/app.js
+++ app/app.js
@@ -3,4 +3,14 @@ angular.module('AiB',[
     'ui.router',
     'ngAnimate',
     'AiB.pages'
-]);
+])
+    .controller('indexCtrl', indexCtrl)
+    .run([
+    function(){
+        if(sessionStorage.getItem('redir') == 'true')
+            window.location.href="welcome.html";
+    }]);
+
+function indexCtrl(){
+    var vm = this;
+}
